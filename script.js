cba1bae2cf6a934fe6ab10684a86613a3b3ba44e
unknown <saritadh74@gmail.com>
2022-04-24 16:56:46 +0300

    In script file I added a function which makes the header change colors from purple to white  when the user scrolls down the page relates #2

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//hello
+//Change the colors of header when the user scrolls down
+window.addEventListener('scroll', function () {
+  let header = document.querySelector('header');
+  header.classList.toggle('scrolled', window.scrollY > 0);
+});
